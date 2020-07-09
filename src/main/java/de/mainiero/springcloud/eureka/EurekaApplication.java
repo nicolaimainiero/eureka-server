f88a8a92473d82d48750303d70813c268ae53ec8
Nicolai Mainiero <nicolai@mainiero.de>
2020-07-09 10:07:31 +0200

    Update EurekaApplication.java
    Add @EnableEurekaServer annotation

--- src/main/java/de/mainiero/springcloud/eureka/EurekaApplication.java
+++ src/main/java/de/mainiero/springcloud/eureka/EurekaApplication.java
@@ -3,6 +3,7 @@ package de.mainiero.springcloud.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
 
